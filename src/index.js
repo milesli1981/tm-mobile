cc6dd1bce9a72b9cdf3d9ab5d682c9aa49b34c52
miles <milesli1981@icloud.com>
2016-08-16 16:52:43 +0800

    change on index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'antd-mobile/lib/button/style';
-import Button from 'antd-mobile/lib/button';
-
-const mountNode = document.getElementById('root');
-import { Tabs, WhiteSpace } from 'antd-mobile';
+import { ListView, Toast, Tabs, WhiteSpace, Icon, Collapse, List, Badge, Flex, Button, WingBlank } from 'antd-mobile';
 const TabPane = Tabs.TabPane;
+const mountNode = document.getElementById('root');
 
-function callback(key) {
-    console.log(key);
-}
 
 let TabExample = React.createClass({
     render() {
         return (
+			
+		
             <div>
-                <Tabs defaultActiveKey="1" onChange={callback}>
-                    <TabPane tab="选项卡A" key="1">这只是个DEMO</TabPane>
-                    <TabPane tab="选项卡B" key="2">只是为了验证antd mobile</TabPane>
-                    <TabPane tab="选项卡C" key="3">感觉antd mobile还不是很成熟, 兼容性有问题, 有些示例也有问题</TabPane>
-                </Tabs>
-                <WhiteSpace />
+                <Tabs type="tabbar" defaultActiveKey="1">
+				  <TabPane tab={<span><Icon type="home" key="1" />SUMMARY</span>}>
+				  <Collapse defaultActiveKey="0">
+					  <Collapse.Panel header="新增任务汇总 (1200/1500)">
+						<List.Item>二次激活  10</List.Item>
+						<List.Item>三次激活  20</List.Item>
+						<List.Item>四次激活  30</List.Item>
+						<List.Item>五次激活  40</List.Item>
+					  </Collapse.Panel>
+					  <Collapse.Panel header="留存任务汇总 (1200/1500)">
+						<List.Item>二次激活  10</List.Item>
+						<List.Item>三次激活  20</List.Item>
+						<List.Item>四次激活  30</List.Item>
+						<List.Item>五次激活  40</List.Item>
+					  </Collapse.Panel>
+					  <Collapse.Panel header="slaver状态 (2/6)">
+						<List.Item>
+							<Flex>
+								<Flex.Item>
+								  <span>slaver-01 (0)</span>
+								</Flex.Item>
+								<Flex.Item>
+								  <Button type="primary">reboot</Button>
+								</Flex.Item>
+								<Flex.Item>
+								  <Button type="primary">release</Button>
+								</Flex.Item>
+							</Flex>
+						</List.Item>
+						
+					  </Collapse.Panel>
+					  <List>
+					  </List>
+				  </Collapse>
+				  </TabPane>
+				  <TabPane tab={<span><Icon type="team" />JOBS</span>} key="2">选项好友内容</TabPane>
+				  <TabPane tab={<span><Icon type="setting" />SLAVERS</span>} key="3">选项设置的内容</TabPane>
+				</Tabs>
+				
             </div>
         );
     },
